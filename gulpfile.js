af756e0af4c7a2070e88d18a890cab95ca0e03cc
Lukas Ruebbelke <simpul@gmail.com>
2015-08-26 00:37:28 -0700

    massive overhaul for the presentation
    dialed in the tasks against sitespeed

--- gulpfile.js
+++ gulpfile.js
@@ -4,74 +4,108 @@ var gulp = require('gulp'),
   clean = require('gulp-clean'),
   concat = require('gulp-concat'),
   inject = require('gulp-inject'),
-  imagemin = require('gulp-imagemin');
+  imagemin = require('gulp-imagemin'),
+  uglify = require('gulp-uglify'),
+  minifyCss = require('gulp-minify-css'),
+  minifyHTML = require('gulp-minify-html'),
+  inline = require('gulp-inline'),
+  sitespeedio = require('gulp-sitespeedio');
 
+
+//-------------------------------------------------------------------
+// PERFORMANCE TASKS
+//-------------------------------------------------------------------
+gulp.task('analyze', function (callback) {
+  runSequence('clean:performance',
+    'sitespeedio',
+    callback
+  )
+});
+
+gulp.task('sitespeedio', sitespeedio({
+    urls: ['http://localhost:3000'],
+    depth: 1,
+    connection: 'cable',
+    resultBaseDir: 'performance',
+    verbose: true,
+    html: true
+  }
+));
+
+gulp.task('clean:performance', function () {
+  return gulp.src('./performance/').pipe(clean());
+});
+
+//-------------------------------------------------------------------
+// BUILD TASKS
+//-------------------------------------------------------------------
 gulp.task('default', function (callback) {
-  runSequence('clean:dev',
-    'copy:dev',
-    'index:dev',
-    'serve:dev',
+  runSequence('clean',
+    'copy',
+    'scripts',
+    'styles',
+    'imagemin',
+    'inject',
+    'inline',
+    'html',
+    'serve',
     callback
   );
 });
 
-gulp.task('compile',function(callback){
-    runSequence('clean:prod',
-      'copy:prod',
-      'concat:scripts',
-      'concat:styles',
-      'index:prod',
-      callback
-    );
-});
-
-gulp.task('imagemin', ['clean'], function() {
-  gulp.src(['./src/images/**/*'], {cwd: './src'})
-    .pipe(imagemin())
-    .pipe(gulp.dest('./build/images/'));
-});
-
-gulp.task('serve:dev', serve('build'));
-gulp.task('serve:prod', serve('bin'));
-
-gulp.task('clean:dev',function(){
-    return gulp.src('./build/').pipe(clean());
-});
-
-gulp.task('clean:prod',function(){
+gulp.task('clean', function () {
   return gulp.src('./bin/').pipe(clean());
 });
 
-gulp.task('copy:dev', function () {
-  return gulp.src(['./src/**/*'])
-    .pipe(gulp.dest('./build'));
-});
-
-gulp.task('copy:prod', function () {
-  return gulp.src(['./build/**/*', '!./build/css/**/*', '!./build/js/**/*'])
+gulp.task('copy', function () {
+  return gulp.src(['./src/**/*', '!./src/css/**/*', '!./src/js/**/*'])
     .pipe(gulp.dest('./bin'));
 });
 
-gulp.task('concat:scripts', function(){
-  return gulp.src(['./build/**/*.js'])
+gulp.task('scripts', function () {
+  return gulp.src([
+      './src/js/jquery-2.1.4.min.js',
+      './src/js/bootstrap.js',
+      './src/js/*.js'
+    ])
     .pipe(concat('scripts.js'))
+    .pipe(uglify())
     .pipe(gulp.dest('./bin/js'));
 });
 
-gulp.task('concat:styles', function(){
-  return gulp.src(['./build/**/*.css'])
+gulp.task('styles', function () {
+  return gulp.src([
+      './src/css/bootstrap.css',
+      './src/css/bootstrap-responsive.css',
+      './src/css/*.css'
+    ])
     .pipe(concat('styles.css'))
+    .pipe(minifyCss())
     .pipe(gulp.dest('./bin/css'));
 });
 
-gulp.task('index:dev', function () {
-  return gulp.src('./build/index.html')
-    .pipe(inject(gulp.src(['./build/**/*.css', './build/**/*.js'], {read: false}), {ignorePath: 'build', addRootSlash: false}))
-    .pipe(gulp.dest('./build'));
+gulp.task('html',function(){
+    return gulp.src(['./bin/*.html'])
+      .pipe(minifyHTML())
+      .pipe(gulp.dest('./bin/'));
 });
 
-gulp.task('index:prod', function () {
+gulp.task('imagemin', function () {
+  return gulp.src('./src/images/**/*')
+    .pipe(imagemin({progressive: true}))
+    .pipe(gulp.dest('./bin/images/'));
+});
+
+gulp.task('inline',function(){
+    return gulp.src('./bin/index.html')
+      .pipe(inline({base:'bin/'}))
+      .pipe(gulp.dest('./bin/'));
+});
+
+gulp.task('inject', function () {
   return gulp.src('./bin/index.html')
     .pipe(inject(gulp.src(['./bin/**/*.css', './bin/**/*.js'], {read: false}), {ignorePath: 'bin', addRootSlash: false}))
     .pipe(gulp.dest('./bin'));
 });
+
+gulp.task('serve', serve('bin'));
